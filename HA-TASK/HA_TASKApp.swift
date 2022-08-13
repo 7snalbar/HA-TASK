4b500435a4a5ec9c4b4fb98c0ba48d19402fa6f5
Hasan Armoush (DEV) <hasan.armoush@nadsoft.com>
2022-08-13 12:08:26 +0300

    Initial Commit

---
+++ HA-TASK/HA_TASKApp.swift
@@ -0,0 +1,17 @@
+//
+//  HA_TASKApp.swift
+//  HA-TASK
+//
+//  Created by Hasan Armoush on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct HA_TASKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
